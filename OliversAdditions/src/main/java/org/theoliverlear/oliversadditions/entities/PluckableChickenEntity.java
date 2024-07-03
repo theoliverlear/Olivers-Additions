c28c84c66aca659ee800d0c81f02aad014050530
theoliverlear <sigwartholiver2@gmail.com>
2024-07-03 18:04:08 -0500

    Refactor PluckableChickenEntity.java, add logging, and streamline pluck check logic

--- OliversAdditions/src/main/java/org/theoliverlear/oliversadditions/entities/PluckableChickenEntity.java
+++ OliversAdditions/src/main/java/org/theoliverlear/oliversadditions/entities/PluckableChickenEntity.java
@@ -1,23 +1,22 @@
 package org.theoliverlear.oliversadditions.entities;
 
+import lombok.Getter;
+import lombok.Setter;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.ChickenEntity;
 import net.minecraft.world.World;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+
+@Getter
+@Setter
 public class PluckableChickenEntity extends ChickenEntity {
+    private static final Logger LOGGER = LogManager.getLogger();
     private boolean isPlucked = false;
     public PluckableChickenEntity(EntityType<? extends ChickenEntity> chickenType,
                                   World fromWorld) {
         super(chickenType, fromWorld);
+        LOGGER.info("PluckableChickenEntity constructor called");
     }
-    public boolean canPluck() {
-        return !this.isPlucked;
-    }
-    public boolean isPlucked() {
-        return this.isPlucked;
-    }
-    public void setPlucked(boolean isPlucked) {
-        this.isPlucked = isPlucked;
-    }
-
 }
