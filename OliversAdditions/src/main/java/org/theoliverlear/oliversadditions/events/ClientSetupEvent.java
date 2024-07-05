f9d28483e0f295a32abaa01f0d92430b975afc8a
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 16:05:35 -0500

    Add comment headers for better code organization in ClientSetupEvent.java

--- OliversAdditions/src/main/java/org/theoliverlear/oliversadditions/events/ClientSetupEvent.java
+++ OliversAdditions/src/main/java/org/theoliverlear/oliversadditions/events/ClientSetupEvent.java
@@ -1,25 +1,19 @@
 package org.theoliverlear.oliversadditions.events;
-
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.entity.EntityRendererManager;
+//=================================-Imports-==================================
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import org.theoliverlear.oliversadditions.OliversAdditionsMod;
-import org.theoliverlear.oliversadditions.entities.PluckableChickenEntity;
 import org.theoliverlear.oliversadditions.register.EntityRegistration;
 import org.theoliverlear.oliversadditions.render.PluckableChickenRenderer;
 
 @Mod.EventBusSubscriber(modid = OliversAdditionsMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientSetupEvent {
+    //=============================-Methods-==================================
 
-//    @SubscribeEvent
-//    public static void clientSetup(FMLClientSetupEvent event) {
-//        RenderingRegistry.registerEntityRenderingHandler(EntityRegistration.PLUCKABLE_CHICKEN.get(), PluckableChickenRenderer::new);
-//    }
+    //-----------------------On-Client-Setup-Event----------------------------
     @SubscribeEvent
     public static void onClientSetupEvent(FMLClientSetupEvent event) {
         RenderingRegistry.registerEntityRenderingHandler(EntityRegistration.PLUCKABLE_CHICKEN.get(), PluckableChickenRenderer::new);
