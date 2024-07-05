6729c832a03b6d273532f5f1d3d9f6716f2eec34
theoliverlear <sigwartholiver2@gmail.com>
2024-07-04 19:28:19 -0500

    Add ModEventSubscriber.java in OliversAdditions module for handling entity attribute creation events

---
+++ OliversAdditions/src/main/java/org/theoliverlear/oliversadditions/events/ModEventSubscriber.java
@@ -0,0 +1,18 @@
+package org.theoliverlear.oliversadditions.events;
+
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.theoliverlear.oliversadditions.OliversAdditionsMod;
+import org.theoliverlear.oliversadditions.entities.PluckableChickenEntity;
+import org.theoliverlear.oliversadditions.register.EntityRegistration;
+
+@Mod.EventBusSubscriber(modid = OliversAdditionsMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModEventSubscriber {
+
+    // Assumes PluckableChickenEntity.PLUCKABLE_CHICKEN is your registered entity type.
+    @SubscribeEvent
+    public static void onEntityAttributeCreation(EntityAttributeCreationEvent event) {
+        event.put(EntityRegistration.PLUCKABLE_CHICKEN.get(), PluckableChickenEntity.createAttributes().build());
+    }
+}
