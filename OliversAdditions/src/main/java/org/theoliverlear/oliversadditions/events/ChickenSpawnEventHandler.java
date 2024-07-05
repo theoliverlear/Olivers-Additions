1ff51474d872ceabdde69d0e102b2838d5ca7c34
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 16:05:09 -0500

    Add comment headers for better code organization in ChickenSpawnEventHandler.java

--- OliversAdditions/src/main/java/org/theoliverlear/oliversadditions/events/ChickenSpawnEventHandler.java
+++ OliversAdditions/src/main/java/org/theoliverlear/oliversadditions/events/ChickenSpawnEventHandler.java
@@ -1,9 +1,8 @@
 package org.theoliverlear.oliversadditions.events;
-
+//=================================-Imports-==================================
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.ChickenEntity;
-import net.minecraft.util.concurrent.TickDelayedTask;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -14,20 +13,22 @@ import org.theoliverlear.oliversadditions.OliversAdditionsMod;
 import org.theoliverlear.oliversadditions.entities.PluckableChickenEntity;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 @Mod.EventBusSubscriber(modid = OliversAdditionsMod.MODID,
                         bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ChickenSpawnEventHandler {
+    //============================-Constants-=================================
     private static final Logger LOGGER = LogManager.getLogger();
     public static final List<Runnable> PENDING_TASKS = Collections.synchronizedList(new ArrayList<>());
+    //=============================-Methods-==================================
+
+    //-----------------------On-Chicken-Spawn-Event---------------------------
     @SubscribeEvent
     public static void onChickenSpawnEvent(EntityJoinWorldEvent event) {
         LOGGER.info("ChickenSpawnEventHandler.onChickenSpawnEvent called");
         Entity entity = event.getEntity();
-
         if (entity instanceof ChickenEntity && !(entity instanceof PluckableChickenEntity) && !event.getWorld().isClientSide()) {
             LOGGER.info("Entity is ChickenEntity and a PluckableChickenEntity is being spawned");
             Runnable task = () -> {
@@ -37,13 +38,13 @@ public class ChickenSpawnEventHandler {
                 pluckableChicken.setDeltaMovement(chicken.getDeltaMovement());
                 pluckableChicken.setCustomName(chicken.getCustomName());
                 pluckableChicken.setCustomNameVisible(chicken.isCustomNameVisible());
-
                 chicken.level.addFreshEntity(pluckableChicken);
                 chicken.remove();
             };
             PENDING_TASKS.add(task);
         }
     }
+    //------------------------On-Server-Tick-Event----------------------------
     @SubscribeEvent
     public static void onServerTickEvent(TickEvent.ServerTickEvent event) {
         if (event.phase == TickEvent.Phase.END) {
