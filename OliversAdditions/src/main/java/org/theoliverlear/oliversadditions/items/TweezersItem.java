31c86c59ecf375a45559a640a32654f052a8f547
theoliverlear <sigwartholiver2@gmail.com>
2024-07-04 19:36:00 -0500

    Refactor TweezersItem.java to interact with PluckableChickenEntity instead of ChickenEntity and add logging for the pluck process

--- OliversAdditions/src/main/java/org/theoliverlear/oliversadditions/items/TweezersItem.java
+++ OliversAdditions/src/main/java/org/theoliverlear/oliversadditions/items/TweezersItem.java
@@ -5,12 +5,10 @@ import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.entity.passive.ChickenEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.SoundEvents;
+import net.minecraft.util.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.theoliverlear.oliversadditions.entities.PluckableChickenEntity;
 
 import java.util.function.Consumer;
 
@@ -22,22 +20,36 @@ public class TweezersItem extends Item {
     @Override
     public ActionResultType interactLivingEntity(ItemStack stack, PlayerEntity player, LivingEntity target, Hand hand) {
         LOGGER.info("TweezersItem.interactLivingEntity called");
-        if (!player.level.isClientSide && target instanceof ChickenEntity) {
-            ChickenEntity chicken = (ChickenEntity) target;
-            player.level.addFreshEntity(new ItemEntity(player.level, chicken.getX(), chicken.getY(), chicken.getZ(), new ItemStack(Items.FEATHER)));
-            // Decrease item durability
-            stack.setDamageValue(stack.getDamageValue() + 1);
-            if (stack.getDamageValue() >= stack.getMaxDamage()) {
-                player.level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ITEM_BREAK, SoundCategory.PLAYERS, 1.0F, 1.0F);
-                stack.shrink(1);
+        if (!player.level.isClientSide && target instanceof PluckableChickenEntity) {
+            LOGGER.info("TweenzersItem interacted with PluckableChickenEntity");
+            PluckableChickenEntity chicken = (PluckableChickenEntity) target;
+
+            // Check if Chicken can be plucked
+            if (chicken.getPluckCount() < PluckableChickenEntity.MAX_PLUCK_COUNT) {
+                LOGGER.info("PluckableChickenEntity can be plucked");
+                // Do the plucking and play the sound
+                chicken.pluck();
+                LOGGER.info("PluckableChickenEntity plucked");
+                player.level.addFreshEntity(new ItemEntity(player.level, chicken.getX(), chicken.getY(), chicken.getZ(), new ItemStack(Items.FEATHER)));
+                player.level.playSound(null, player.getX(), player.getY(), player.getZ(),
+                        new SoundEvent(new ResourceLocation("olivers_additions:pluck")),
+                        SoundCategory.PLAYERS, 1.0F, 1.0F);
+
+                // Decrease item durability
+                stack.setDamageValue(stack.getDamageValue() + 1);
+                if (stack.getDamageValue() >= stack.getMaxDamage()) {
+                    player.level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ITEM_BREAK, SoundCategory.PLAYERS, 1.0F, 1.0F);
+                    stack.shrink(1);
+                }
+
+                return ActionResultType.SUCCESS;
             }
-            return ActionResultType.SUCCESS;
         }
         return super.interactLivingEntity(stack, player, target, hand);
     }
     @Override
     public <T extends LivingEntity> int damageItem(ItemStack stack, int amount, T entity, Consumer<T> onBroken) {
-        if (entity instanceof PlayerEntity && ((PlayerEntity)entity).abilities.instabuild) {
+        if (entity instanceof PlayerEntity && ((PlayerEntity) entity).abilities.instabuild) {
             return 0;
         }
         if (this.isDamageable(stack)) {
