2014f4e3b6c70dc35a57b8f07c2fde83ccc304fe
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 16:09:43 -0500

    Add comment headers for better code organization, remove forge generated comments, and refactor method names to be more precise in TweezersItem.java

--- OliversAdditions/src/main/java/org/theoliverlear/oliversadditions/OliversAdditionsMod.java
+++ OliversAdditions/src/main/java/org/theoliverlear/oliversadditions/OliversAdditionsMod.java
@@ -1,8 +1,6 @@
 package org.theoliverlear.oliversadditions;
-
+//=================================-Imports-==================================
 import net.minecraft.block.Blocks;
-import net.minecraft.client.renderer.entity.EntityRendererManager;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -24,63 +22,55 @@ import org.theoliverlear.oliversadditions.render.PluckableChickenRenderer;
 
 import java.util.stream.Collectors;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(OliversAdditionsMod.MODID)
 public class OliversAdditionsMod {
+    //============================-Constants-=================================
     public static final String MODID = "olivers_additions";
-    // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
+    //===========================-Constructors-===============================
     public OliversAdditionsMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         ItemRegistration.register(modEventBus);
         EntityRegistration.register(modEventBus);
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::initSetup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::dispatchInterModComms);
-        // Register the processIMC method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processInterModComms);
-        // Register the doClientStuff method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::handleClientSetup);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-
-        // Register ourselves for server and other game events we are interested in
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientRegistration);
         MinecraftForge.EVENT_BUS.register(this);
     }
-    private void doClientStuff(final FMLClientSetupEvent event) {
+    //=============================-Methods-==================================
+
+    //------------------------Client-Registration-----------------------------
+    private void clientRegistration(final FMLClientSetupEvent event) {
         LOGGER.info("CREATING PLUCKABLE CHICKEN RENDERER");
         RenderingRegistry.registerEntityRenderingHandler(EntityRegistration.PLUCKABLE_CHICKEN.get(), PluckableChickenRenderer::new);
     }
-    private void setup(final FMLCommonSetupEvent event) {
+    //-----------------------------Init-Setup---------------------------------
+    private void initSetup(final FMLCommonSetupEvent event) {
         // some preinit code
         LOGGER.info("HELLO FROM PREINIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
+    //------------------------Handle-Client-Setup-----------------------------
     private void handleClientSetup(final FMLClientSetupEvent event) {
-        // do something that can only be done on the client
         LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
     }
+    //------------------Dispatch-Intermod-Communication-----------------------
     private void dispatchInterModComms(final InterModEnqueueEvent event) {
-        // some example code to dispatch IMC to another mod
         InterModComms.sendTo("olivers_additions", "helloworld",
                 () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
+    //-------------------Process-Intermod-Communication-----------------------
     private void processInterModComms(final InterModProcessEvent event) {
-        // some example code to receive and process InterModComms from other mods
         LOGGER.info("Got IMC {}", event.getIMCStream().
                 map(m->m.getMessageSupplier().get()).
                 collect(Collectors.toList()));
     }
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    //-------------------------On-Server-Starting-----------------------------
     @SubscribeEvent
     public void onServerStarting(FMLServerStartingEvent event) {
-        // do something when the server starts
         LOGGER.info("HELLO from server starting");
         HelloWorldCommand.registerHelloWorldCommand(event.getServer().getCommands().getDispatcher());
-        // Register tweezers item
-
     }
-    // You can use EventBusSubscriber to automatically subscribe events
-    // on the contained class (this is subscribing to the MOD Event bus for
-    // receiving Registry Events).
 }
