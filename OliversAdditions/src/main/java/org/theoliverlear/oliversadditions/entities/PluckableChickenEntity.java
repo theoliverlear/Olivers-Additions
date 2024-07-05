7cc285711406c694816ba421101fd296d0e39d0d
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 16:07:48 -0500

    Add comment headers for better code organization in PluckableChickenEntity.java

--- OliversAdditions/src/main/java/org/theoliverlear/oliversadditions/entities/PluckableChickenEntity.java
+++ OliversAdditions/src/main/java/org/theoliverlear/oliversadditions/entities/PluckableChickenEntity.java
@@ -1,5 +1,5 @@
 package org.theoliverlear.oliversadditions.entities;
-
+//=================================-Imports-==================================
 import lombok.Getter;
 import lombok.Setter;
 import net.minecraft.entity.EntityType;
@@ -10,22 +10,23 @@ import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
 @Getter
 @Setter
 public class PluckableChickenEntity extends ChickenEntity {
+    //============================-Constants-=================================
     private static final Logger LOGGER = LogManager.getLogger();
-    private boolean isPlucked;
-    private int pluckCount;
-    private int ticksSinceLastPluck;
     public static final int MAX_PLUCK_COUNT = 4;
     public static final int TICKS_TO_GROW_FEATHER = 1200;
     public static final DataParameter<Boolean> IS_PLUCKED = EntityDataManager.defineId(PluckableChickenEntity.class, DataSerializers.BOOLEAN);
+    //============================-Variables-=================================
+    private boolean isPlucked;
+    private int pluckCount;
+    private int ticksSinceLastPluck;
+    //===========================-Constructors-===============================
     public PluckableChickenEntity(EntityType<? extends ChickenEntity> chickenType,
                                   World fromWorld) {
         super(chickenType, fromWorld);
@@ -36,24 +37,9 @@ public class PluckableChickenEntity extends ChickenEntity {
         this.getAttribute(Attributes.MOVEMENT_SPEED).setBaseValue(0.25D);
         LOGGER.info("PluckableChickenEntity constructor called");
     }
-    @Override
-    protected void defineSynchedData() {
-        super.defineSynchedData();
-        this.entityData.define(IS_PLUCKED, false);
-    }
-    @Override
-    public void tick() {
-        super.tick();
-        this.ticksSinceLastPluck++;
-        if (this.ticksSinceLastPluck >= TICKS_TO_GROW_FEATHER && this.pluckCount > 0) {
-            this.pluckCount--;
-            this.ticksSinceLastPluck = 0;
-            if (this.isPlucked) {
-                this.isPlucked = false;
-                this.entityData.set(IS_PLUCKED, false);
-            }
-        }
-    }
+    //=============================-Methods-==================================
+
+    //-------------------------------Pluck------------------------------------
     public void pluck() {
         if (this.pluckCount < MAX_PLUCK_COUNT) {
             LOGGER.info("Chicken plucked");
@@ -67,16 +53,39 @@ public class PluckableChickenEntity extends ChickenEntity {
             this.setDeltaMovement(this.getDeltaMovement().add(0, 0.05, 0));
         }
     }
-    public static AttributeModifierMap.MutableAttribute createAttributes() {
-        return ChickenEntity.createAttributes(); // defaults to ChickenEntity attributes; customize as needed
+    //============================-Overrides-=================================
+    @Override
+    protected void defineSynchedData() {
+        super.defineSynchedData();
+        this.entityData.define(IS_PLUCKED, false);
     }
+    //--------------------------------Tick------------------------------------
+    @Override
+    public void tick() {
+        super.tick();
+        this.ticksSinceLastPluck++;
+        if (this.ticksSinceLastPluck >= TICKS_TO_GROW_FEATHER && this.pluckCount > 0) {
+            this.pluckCount--;
+            this.ticksSinceLastPluck = 0;
+            if (this.isPlucked) {
+                this.isPlucked = false;
+                this.entityData.set(IS_PLUCKED, false);
+            }
+        }
+    }
+    //-------------------------Create-Attributes------------------------------
+    // Overridden but not annotated with @Override.
+    public static AttributeModifierMap.MutableAttribute createAttributes() {
+        return ChickenEntity.createAttributes();
+    }
+    //----------------------Add-Additional-Save-Data--------------------------
     @Override
     public void addAdditionalSaveData(CompoundNBT compound) {
         super.addAdditionalSaveData(compound);
         compound.putBoolean("isPlucked", this.isPlucked);
         compound.putInt("pluckCount", this.pluckCount);
     }
-
+    //---------------------Read-Additional-Save-Data--------------------------
     @Override
     public void readAdditionalSaveData(CompoundNBT compound) {
         super.readAdditionalSaveData(compound);
