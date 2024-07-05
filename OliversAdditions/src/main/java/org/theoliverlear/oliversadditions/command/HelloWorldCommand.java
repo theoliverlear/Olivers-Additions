b9152347397aa920c544501f9fbb41c77c48f4c1
theoliverlear <sigwartholiver2@gmail.com>
2024-07-05 16:06:15 -0500

    Add comment headers for better code organization in HelloWorldCommand.java

--- OliversAdditions/src/main/java/org/theoliverlear/oliversadditions/command/HelloWorldCommand.java
+++ OliversAdditions/src/main/java/org/theoliverlear/oliversadditions/command/HelloWorldCommand.java
@@ -1,12 +1,14 @@
 package org.theoliverlear.oliversadditions.command;
-
+//=================================-Imports-==================================
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.tree.LiteralCommandNode;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
 import net.minecraft.util.text.StringTextComponent;
 
 public class HelloWorldCommand {
+    //=============================-Methods-==================================
+
+    //--------------------Register-Hello-World-Command------------------------
     public static void registerHelloWorldCommand(CommandDispatcher<CommandSource> cmdDispatcher) {
         cmdDispatcher.register(Commands.literal("helloworld")
                               .executes(cmdExecution -> {
